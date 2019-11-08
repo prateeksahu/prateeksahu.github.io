ca037e651d466cd751c1daa049dd121c4baadca5
Prateek Sahu <prateeks@utexas.edu>
2019-11-08 14:59:22 -0600

    update all class materials

--- ent_sec/file/class/printf.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main(void)
-{
-    int i = 999;
-    printf("This is a test %d", i);
-}
--- ent_sec/file/class/write.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-void main(void)
-{
-    write(1, "This is a test\n", 15);
-}
